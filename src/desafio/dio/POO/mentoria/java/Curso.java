90fde1cf51fe1b764a8cda5048579107d9a71395
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2023-01-06 12:39:04 -0300

    "Modificação no toString "

--- src/desafio/dio/POO/mentoria/java/Curso.java
+++ src/desafio/dio/POO/mentoria/java/Curso.java
@@ -21,10 +21,9 @@ public class Curso extends Conteudo {
 
     @Override
     public String toString() {
-        return "Curso{" +
-                "titulo='" + getTitulo() + '\'' +
-                ", descricao='" + getDescricao()+ '\'' +
-                ", cargaHoraria=" + cargaHoraria +
-                '}';
+        return "\nCurso:\n" +
+                "titulo= " + getTitulo() +
+                "\ndescricao= " + getDescricao()+
+                "\ncargaHoraria= " + cargaHoraria + "\n-----------------------------";
     }
 }
--- src/desafio/dio/POO/mentoria/java/Mentoria.java
+++ src/desafio/dio/POO/mentoria/java/Mentoria.java
@@ -23,10 +23,9 @@ public class Mentoria extends Conteudo {
 
     @Override
     public String toString() {
-        return "Mentoria{" +
-                "titulo='" + getTitulo() + '\'' +
-                ", descricao='" + getDescricao() + '\'' +
-                ", data=" + data +
-                '}';
+        return "\nMentoria:\n" +
+                "titulo = " + getTitulo() +
+                "\ndescricao =" + getDescricao() +
+                "\ndata = " + data +"\n-----------------------------";
     }
 }
